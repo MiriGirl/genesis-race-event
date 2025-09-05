388606fc2538da3c996bf130e760b6eeac9f2666
Miriam Kumar <miriam@Miriams-MacBook-Pro.local>
2025-09-06 03:02:58 +0530

    fix: update F-link to vercel domain

--- src/app/[fno]/page.tsx
+++ src/app/[fno]/page.tsx
@@ -1,4 +1,4 @@
-import RacePageClient from "./racepageclient.jsx";
+import RacePageClient from "./racepageclient";
 
 export default function RacePage({ params }: { params: { fno: string } }) {
   // Always render the client component with whatever FNO is passed
